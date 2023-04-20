57331a418cecf8c45cf6aa2896c9bce2db4a085b
Zackaria Antar <antarzack@gmail.com>
2023-04-20 17:28:12 -0500

    Answered the first 3 questions

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,15 +15,17 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(helloName('Zackaria'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber) {
+  let answer = firstNumber + secondNumber;
+  return answer;
 }
+console.log(addNumbers(9 ,10));
 
 
 // 4. Function to multiply three numbers & return the result
